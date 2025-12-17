0039c775b2ec28c185e38aea0ab641da3386f96d
ines312692 <ines.tmimi@ensit.u-tunis.tn>
2025-12-17 12:54:03 +0100

    refactor 3Sum algorithm to use Counter for improved performance and clarity

--- Problem_Solving/3sum.py
+++ Problem_Solving/3sum.py
@@ -1,16 +1,30 @@
-from typing import List
+from collections import Counter
 
 
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        result = set()  # set pour éviter doublons
+    def threeSum(self, nums):
+        f = Counter(nums)
+        res = set()
+        if f[0] >= 3:
+            res.add((0, 0, 0))
 
-        for i in range(n):
-            for j in range(i + 1, n):
-                for k in range(j + 1, n):
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        triplet = tuple(sorted([nums[i], nums[j], nums[k]]))
-                        result.add(triplet)
+        neg = [x for x in f if x < 0]
+        pos = [x for x in f if x > 0]
 
-        return [list(triplet) for triplet in result]
+        for i in neg:
+            for j in pos:
+                k = -i - j
+                if k in f:
+                    if (k == i or k == j) and f[k] < 2:
+                        continue
+                    if k == i and k == j and f[k] < 3:
+                        continue
+                    res.add(tuple(sorted((i, j, k))))
+
+        for x in f:
+            if f[x] >= 2:
+                y = -2 * x
+                if y != x and y in f:
+                    res.add(tuple(sorted((x, x, y))))
+
+        return [list(t) for t in res]
